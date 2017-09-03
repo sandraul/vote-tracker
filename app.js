c1b3fd113124436aa84e162749649c251d091cf2
Sandra <sandraultreras@gmail.com>
2017-09-03 12:42:41 -0700

    Got list to show after 15 clicks

--- app.js
+++ app.js
@@ -1,59 +1,33 @@
-//===============Creating a Vote Tracker for random images====================
 
 //================Constructor function==================
 //Create a Constructor function for all the products BusMall sells
 var clickTotal = 0;
 
-var ImageOption = function(name) {
+var ImageOption = function(name, ) {
   this.name = name;
   this.tally = 0;
   this.fileName = name;
+  this.label = name; //Add this line when building chart
+  this.y = Math.floor(Math.random() * images.length); //Add this line when building chart
   }
 
 var images = []; //Array to hold all of the ImageOption instances
-images.push(new ImageOption("bag.jpg"));
-images.push(new ImageOption("banana.jpg"));
-images.push(new ImageOption("boots.jpg"));
-images.push(new ImageOption("chair.jpg"));
-images.push(new ImageOption("cthulhu.jpg"));
-images.push(new ImageOption("dragon.jpg"));
-images.push(new ImageOption("pen.jpg"));
-images.push(new ImageOption("scissors.jpg"));
-images.push(new ImageOption("shark.jpg"));
-images.push(new ImageOption("sweep.jpg"));
-images.push(new ImageOption("unicorn.jpg"));
-images.push(new ImageOption("usb.jpg"));
-images.push(new ImageOption("water_can.jpg"));
-images.push(new ImageOption("wine_glass.jpg"));
+images.push(new ImageOption("bag.jpg", "Bag"));
+images.push(new ImageOption("banana.jpg", "Banana"));
+images.push(new ImageOption("boots.jpg", "Boots"));
+images.push(new ImageOption("chair.jpg", "Chair"));
+images.push(new ImageOption("cthulhu.jpg", "Cthulhu"));
+images.push(new ImageOption("dragon.jpg", "Dragon"));
+images.push(new ImageOption("pen.jpg", "Pen"));
+images.push(new ImageOption("scissors.jpg", "Scissors"));
+images.push(new ImageOption("shark.jpg", "Shark"));
+images.push(new ImageOption("sweep.jpg", "Sweep"));
+images.push(new ImageOption("unicorn.jpg", "Unicorn"));
+images.push(new ImageOption("usb.jpg", "USB"));
+images.push(new ImageOption("water_can.jpg", "Water Can"));
+images.push(new ImageOption("wine_glass.jpg", "Wine Glass"));
 
 // console.log(images);
-
-//Make a function to record number of clicks of each image
-function recordClick(event) {
-  images[event.target.dataset.index].tally++;
-
-  clickTotal++; //this is the same as clickTotal = clickTotal +1
-  //When reach maximum clicks remove the event listener
-  if (clickTotal == 15) {
-    // document.getElementById("image-container").innerHTML = "";
-    var product = document.getElementById("result");
-    product.removeEventListener("click", recordClick);
-  }
-  else {
-    showImages();
-  }
-
-  //After the 15th click load click tracker on list
-  var list = document.getElementById("favorite-list");
-  for (var faves = 0; faves < images.length; faves++) {
-    var li = document.createElement("li");
-    li.innerText = images[faves].name + " was clicked " + images[faves].tally + " times";
-    //Append the list items into the html list
-    list.appendChild(li);
-  }
-  console.log(images[event.target.dataset.index]);
-}
-
 
 //Creates image container for html
 function addImage(imageFileName, index) {
@@ -67,29 +41,58 @@ function addImage(imageFileName, index) {
   container.appendChild(image);
 }
 
-
 //Make a function to randomly choose 3 images from images array
 function showImages() {
   var index1 = Math.floor(Math.random() * images.length);
   var index2 = Math.floor(Math.random() * images.length);
   var index3 = Math.floor(Math.random() * images.length);
 
-//Check that the 3 images are different on every load
+  //Check that the 3 images are different on every load
   while (index1 == index2 || index2 == index3 || index1 == index3) {
     index1 = Math.floor(Math.random() * images.length);
     index2 = Math.floor(Math.random() * images.length);
     index3 = Math.floor(Math.random() * images.length);
   }
 
-// console.log(images[index1]);
+  // console.log(images[index1]);
 
-//Clears the HTML after each click so it can show 3 new random images
+  //Clears the HTML after each click so it can show 3 new random images
   document.getElementById("image-container").innerHTML = "";
 
   //Add 3 random image to the Html container
   addImage("pictures/"+images[index1].fileName, index1);
   addImage("pictures/"+images[index2].fileName, index2);
   addImage("pictures/"+images[index3].fileName, index3);
+}
+
+//Make a function to record number of clicks of each image
+//getAttribute() returns the value of a specified attribute
+//on the element. If the given attribute does not exist,
+//the value returned will either be null or "" (the empty string
+
+function recordClick(event) {
+  images[event.target.dataset.index].tally++;
+
+  clickTotal++; //this is the same as clickTotal = clickTotal +1
+  //When reach maximum clicks remove the event listener
+  if (clickTotal == 15) {
+    document.getElementById("image-container").innerHTML = "";
+    // var product = document.getElementById("result");
+    // product.removeEventListener("click", recordClick);
+    //After the 15th click load click tracker on list
+    var list = document.getElementById("faves-list");
+    for (var i = 0; i < images.length; i++) {
+      var li = document.createElement("li");
+      li.innerText = images[i].name + " was clicked " + images[i].tally + " times";
+      //Append the list items into the html list
+      list.appendChild(li);
+    }
+  }
+  else {
+    showImages();
+  }
+
+  console.log(images[event.target.dataset.index]);
 }
 
 window.addEventListener("load", showImages);
